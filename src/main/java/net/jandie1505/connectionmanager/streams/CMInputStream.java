1b343e286e3ce5adb0b4459a847b0eded9ff0fe9
jandie1505 <34031551+jandie1505@users.noreply.github.com>
2022-04-15 21:51:25 +0200

    Fixed CMInputStream

--- src/main/java/net/jandie1505/connectionmanager/streams/CMInputStream.java
+++ src/main/java/net/jandie1505/connectionmanager/streams/CMInputStream.java
@@ -12,38 +12,56 @@ public class CMInputStream extends InputStream {
     private List<Integer> queue;
     private CountDownLatch latch;
     private Thread thread;
+    private int value;
+    private long time;
 
     public CMInputStream(ThreadStopCondition client) {
+        this.latch = new CountDownLatch(1);
         this.owner = client;
         this.queue = new ArrayList<>();
+        this.value = -2;
+        this.time = 10;
 
         this.thread = new Thread(() -> {
             while(!Thread.currentThread().isInterrupted() && !this.owner.isClosed()) {
                 if(this.latch != null && this.latch.getCount() != 0 && this.queue.size() > 0) {
+                    this.value = this.queue.remove(0);
                     this.latch.countDown();
+                    this.latch = new CountDownLatch(1);
                     try {
-                        Thread.sleep(1);
-                        this.queue.remove(0);
-                    } catch (IndexOutOfBoundsException | InterruptedException ignored) {
+                        Thread.sleep(this.time);
+                    } catch (InterruptedException ignored) {
                         //
                     }
                 }
+            }
+            this.value = -1;
+            if(this.latch != null) {
+                this.latch.countDown();
             }
         });
         this.thread.setName(this + "-Thread");
         this.thread.start();
     }
 
+    /**
+     * Set the time interval for sending the next byte to all threads that called the read() method.
+     * @param millis Time in milliseconds
+     */
+    public void setTimeInterval(long millis) {
+        this.time = millis;
+    }
+
+    /**
+     * This method is blocking the current thread until a byte is received.
+     * @return byte int
+     */
     @Override
     public int read() {
         try {
-            if(this.latch == null) {
-                this.latch = new CountDownLatch(1);
-            }
-            if(!this.thread.isInterrupted()) {
+            if(!this.thread.isInterrupted() && !this.owner.isClosed()) {
                 this.latch.await();
-                this.latch = null;
-                return this.queue.remove(0);
+                return value;
             } else {
                 return -1;
             }
@@ -53,6 +71,12 @@ public class CMInputStream extends InputStream {
         return -1;
     }
 
+    /**
+     * DO NOT USE THIS!
+     * @param b byte int (0-255)
+     * @deprecated DO NOT USE THIS!
+     */
+    @Deprecated
     public void send(int b) {
         if(b >= 0 && this.latch != null) {
             this.queue.add(b);
